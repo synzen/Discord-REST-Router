aab46cfe7755ebcf0bb2ff323169f3592e8af3ed
synzen <michael994664@gmail.com>
2020-08-29 17:55:23 -0400

    Set app logger to file in production

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,13 +1,36 @@
 import winston from 'winston'
+import isProductionEnv from './isProductionEnv'
 
-const log = winston.createLogger({
+const productionLog = winston.createLogger({
+  transports: [
+    new winston.transports.File({
+      filename: 'combined.log',
+      level: 'info'
+    }),
+    new winston.transports.File({
+      filename: 'error.log',
+      level: 'error'
+    }),
+    new winston.transports.Console({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.simple(),
+        winston.format.timestamp()
+      )
+    })
+  ]
+})
+
+const devLog = winston.createLogger({
   transports: [
     new winston.transports.Console(),
   ],
   format: winston.format.combine(
-    winston.format.prettyPrint(),
-    winston.format.simple()
+    winston.format.simple(),
+    winston.format.timestamp()
   )
 })
+
+const log = isProductionEnv ? productionLog : devLog
 
 export default log
