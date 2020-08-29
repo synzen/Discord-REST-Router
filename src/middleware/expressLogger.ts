a710758648f842632e40d137b7ec7e9e822836c1
synzen <michael994664@gmail.com>
2020-08-29 15:49:07 -0400

    Always log body in non-prod logs

--- src/middleware/expressLogger.ts
+++ src/middleware/expressLogger.ts
@@ -5,10 +5,6 @@ import isProductionEnv from "../utils/isProductionEnv";
 import log from "../utils/log";
 import requestIp from 'request-ip'
 
-function is200Status (status: number) {
-  return status.toString().startsWith('2')
-}
-
 const expressLogger = (req: Request, res: Response, next: NextFunction) => {
   if (isProductionEnv) {
     expressWinston.logger({
@@ -24,9 +20,7 @@ const expressLogger = (req: Request, res: Response, next: NextFunction) => {
   } else {
     res.once('finish', () => {
       const ip = requestIp.getClientIp(req)
-      log.info(`${ip} ${res.statusCode} ${req.originalUrl} | body: ${is200Status(res.statusCode)
-        ? 'Hidden on OK status'
-        : JSON.stringify(req.body, null, 2)}`)
+      log.info(`${ip} ${res.statusCode} ${req.originalUrl} | body: ${JSON.stringify(req.body, null, 2)}`)
     })
     next()
   }
